3a2b3447ca0017b3dfda16bd9318bd4726080cf2
ygao602 <ygao602@gatech.edu>
2026-06-08 14:57:28 -0400

    parking lot LLD

--- ParkingLot/main.py
+++ ParkingLot/main.py
@@ -25,12 +25,15 @@ class main:
         car1 = Vehicle(VehicleType.CAR, "ABC1122")
         bike1 = Vehicle(VehicleType.BIKE, "AACC13")
         truck1 = Vehicle(VehicleType.TRUCK, "1234567")
+        car2 = Vehicle(VehicleType.CAR, "AAA1111")
 
         parking_lot.park_vehicle(car1)
         parking_lot.park_vehicle(bike1)
         parking_lot.park_vehicle(truck1)
+        parking_lot.park_vehicle(car2)
 
         parking_lot.unpark_vehicle(car1)
+        
 
     if __name__ == "__main__":
         main()
